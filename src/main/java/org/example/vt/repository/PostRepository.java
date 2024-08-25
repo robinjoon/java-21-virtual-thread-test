b9c151a780eaede7ad7b372a0bc067f389ea361e
임수빈 <robin980108@naver.com>
2024-08-26 00:52:34 +0900

    필요없는 메서드 오버라이딩 제거

--- src/main/java/org/example/vt/repository/PostRepository.java
+++ src/main/java/org/example/vt/repository/PostRepository.java
@@ -1,10 +1,7 @@
 package org.example.vt.repository;
 
 import org.example.vt.entity.Post;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Page<Post> findAll(Pageable pageable);
 }
