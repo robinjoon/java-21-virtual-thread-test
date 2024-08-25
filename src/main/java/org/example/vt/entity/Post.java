d840385dd74b9d0b73310f455ba3d954324ad03d
임수빈 <robin980108@naver.com>
2024-08-26 00:51:27 +0900

    레이지 로딩으로 변경

--- src/main/java/org/example/vt/entity/Post.java
+++ src/main/java/org/example/vt/entity/Post.java
@@ -1,6 +1,7 @@
 package org.example.vt.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,9 +24,9 @@ public class Post {
     private String title;
     private String content;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Board board;
 }
